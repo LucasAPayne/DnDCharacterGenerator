e9826d2386ce5908b9143838edc8a96774aeaa3d
LucasAPayne <lucasapayne@outlook.com>
2020-09-30 13:01:10 -0400

    Made code and output formatting more consistent

--- DnDCharacterGenerator/src/DnDCharacter.cpp
+++ DnDCharacterGenerator/src/DnDCharacter.cpp
@@ -33,9 +33,7 @@ namespace dnd {
 		std::shuffle(list.begin(), list.end(), Random::GetEngine());
 
 		for (int i = 0; i < x; ++i)
-		{
 			list[i].get().Proficient = true;
-		}
 	}
 
 	Character::Character()
@@ -117,11 +115,7 @@ namespace dnd {
 
 	void Character::GenerateGender()
 	{
-		// Gender needed to generate random name
-		if (m_Gender == "")
-		{
-			m_Gender = Genders[Random::Int(0, NumGenders - 1)];
-		}
+		m_Gender = Genders[Random::Int(0, NumGenders - 1)];
 	}
 
 	void Character::GenerateName()
@@ -193,25 +187,15 @@ namespace dnd {
 	void Character::GenerateProficiencyBonus()
 	{
 		if (m_Level >= 1 && m_Level <= 4)
-		{
 			m_ProficiencyBonus = 2;
-		}
 		else if (m_Level >= 5 && m_Level <= 8)
-		{
 			m_ProficiencyBonus = 3;
-		}
 		else if (m_Level >= 9 && m_Level <= 12)
-		{
 			m_ProficiencyBonus = 4;
-		}
 		else if (m_Level >= 13 && m_Level <= 16)
-		{
 			m_ProficiencyBonus = 5;
-		}
 		else if (m_Level >= 17 && m_Level <= 20)
-		{
 			m_ProficiencyBonus = 6;
-		}
 	}
 
 	void Character::GenerateAbilityScores()
@@ -224,68 +208,46 @@ namespace dnd {
 		for (int i = 0; i < 6; ++i)
 		{
 			for (int j = 0; j < 4; ++j)
-			{
 				results.push_back(Random::Int(1, 6));
-			}
 
 			std::sort(results.begin(), results.end(), std::greater<int>()); // Put the results in order of greatest to least
 			totals.push_back(results[0] + results[1] + results[2]); // Add the greatest three results of this run and put the sum in totals
 		}
 
 		// Assign ability scores to the values in totals
-		m_Strength.Score = totals[0];
-		m_Dexterity.Score = totals[1];
+		m_Strength.Score     = totals[0];
+		m_Dexterity.Score    = totals[1];
 		m_Constitution.Score = totals[2];
 		m_Intelligence.Score = totals[3];
-		m_Wisdom.Score = totals[4];
-		m_Charisma.Score = totals[5];
+		m_Wisdom.Score       = totals[4];
+		m_Charisma.Score     = totals[5];
 	}
 
 	void Character::GenerateRacialAbilityBonuses()
 	{
 		// Subraces also get parent race benefits
 		if (m_Race == "Dwarf" || m_Race == "Hill Dwarf" || m_Race == "Mountain Dwarf")
-		{
 			m_Constitution.Score+= 2;
-		}
 		if (m_Race == "Hill Dwarf")
-		{
 			m_Wisdom.Score++;
-		}
 		if (m_Race == "Mountain Dwarf")
-		{
 			m_Strength.Score+= 2;
-		}
 
 		if (m_Race == "Elf" || m_Race == "High Elf" || m_Race == "Wood Elf" || m_Race == "Dark Elf (Drow)")
-		{
 			m_Dexterity.Score+= 2;
-		}
 		if (m_Race == "High Elf")
-		{
 			m_Intelligence.Score++;
-		}
 		if (m_Race == "Wood Elf")
-		{
 			m_Wisdom.Score++;
-		}
 		if (m_Race == "Dark Elf (Drow)")
-		{
 			m_Charisma.Score++;
-		}
 
 		if (m_Race == "Halfling" || m_Race == "Lightfoot Halfling" || m_Race == "Stout Halfling")
-		{
 			m_Dexterity.Score+= 2;
-		}
 		if (m_Race == "Lightfoot Halfling")
-		{
 			m_Charisma.Score++;
-		}
 		if (m_Race == "Stout Halfling")
-		{
 			m_Constitution.Score++;
-		}
 
 		if (m_Race == "Human")
 		{
@@ -313,20 +275,18 @@ namespace dnd {
 				abilities[i].Modifier = (temp - 1) / 2;
 		}
 
-		m_Strength.Modifier = abilities[0].Modifier;
-		m_Dexterity.Modifier = abilities[1].Modifier;
+		m_Strength.Modifier     = abilities[0].Modifier;
+		m_Dexterity.Modifier    = abilities[1].Modifier;
 		m_Constitution.Modifier = abilities[2].Modifier;
 		m_Intelligence.Modifier = abilities[3].Modifier;
-		m_Wisdom.Modifier = abilities[4].Modifier;
-		m_Charisma.Modifier = abilities[5].Modifier;
+		m_Wisdom.Modifier       = abilities[4].Modifier;
+		m_Charisma.Modifier     = abilities[5].Modifier;
 	}
 
 	void Character::GenerateSkillProficiencies()
 	{
 		if (m_Class == "Barbarian")
-		{
 			chooseXRandomlyFrom(2, { m_AnimalHandling, m_Athletics, m_Intimidation, m_Nature, m_Perception, m_Survival });
-		}
 		if (m_Class == "Bard")
 		{
 			// Pick any three skills
@@ -335,58 +295,38 @@ namespace dnd {
 				m_SleightOfHand, m_Stealth, m_Survival });
 		}
 		if (m_Class == "Cleric")
-		{
 			chooseXRandomlyFrom(2, { m_History, m_Insight, m_Medicine, m_Persuasion, m_Religion });
-		}
 		if (m_Class == "Druid")
-		{
 			chooseXRandomlyFrom(2, { m_Arcana, m_AnimalHandling, m_Insight, m_Medicine, m_Nature, m_Perception, m_Religion, m_Survival });
-		}
 		if (m_Class == "Fighter")
-		{
-			chooseXRandomlyFrom(2, { m_Acrobatics, m_AnimalHandling, m_Athletics, m_History, m_Insight, m_Intimidation, m_Perception,
-				m_Survival });
-		}
+			chooseXRandomlyFrom(2, { m_Acrobatics, m_AnimalHandling, m_Athletics, m_History, m_Insight, m_Intimidation, m_Perception, m_Survival });
 		if (m_Class == "Monk")
-		{
 			chooseXRandomlyFrom(2, { m_Acrobatics, m_Athletics, m_History, m_Insight, m_Religion, m_Stealth });
-		}
 		if (m_Class == "Paladin")
-		{
 			chooseXRandomlyFrom(2, { m_Athletics, m_Insight, m_Intimidation, m_Medicine, m_Persuasion, m_Religion });
-		}
 		if (m_Class == "Ranger")
-		{
-			chooseXRandomlyFrom(3, { m_AnimalHandling, m_Athletics, m_Insight, m_Investigation, m_Nature, m_Perception,
-				m_Stealth, m_Survival });
-		}
+			chooseXRandomlyFrom(3, { m_AnimalHandling, m_Athletics, m_Insight, m_Investigation, m_Nature, m_Perception, m_Stealth, m_Survival });
 		if (m_Class == "Rogue")
 		{
 			chooseXRandomlyFrom(4, { m_Acrobatics, m_Athletics, m_Deception, m_Insight, m_Intimidation,
 				m_Investigation, m_Perception, m_Performance, m_Persuasion, m_SleightOfHand, m_Stealth });
 		}
 		if (m_Class == "Sorceror")
-		{
 			chooseXRandomlyFrom(2, { m_Arcana, m_Deception, m_Insight, m_Intimidation, m_Persuasion, m_Religion });
-		}
 		if (m_Class == "Warlock")
-		{
 			chooseXRandomlyFrom(2, { m_Arcana, m_Deception, m_History, m_Intimidation, m_Investigation, m_Nature, m_Religion });
-		}
 		if (m_Class == "Wizard")
-		{
 			chooseXRandomlyFrom(2, { m_Arcana, m_History, m_Insight, m_Investigation, m_Medicine, m_Religion });
-		}
 	}
 
 	void Character::GenerateSkillModifiers()
 	{
 		// Skill modifiers are based on the abilities that govern them
-		m_Athletics.Parent = m_Strength;
-		m_Acrobatics.Parent = m_SleightOfHand.Parent = m_Stealth.Parent = m_Dexterity;
-		m_Arcana.Parent = m_History.Parent = m_Investigation.Parent = m_Nature.Parent = m_Religion.Parent = m_Intelligence;
-		m_AnimalHandling.Parent = m_Insight.Parent = m_Medicine.Parent = m_Perception.Parent = m_Survival.Parent = m_Wisdom;
-		m_Deception.Parent = m_Intimidation.Parent = m_Performance.Parent = m_Persuasion.Parent = m_Charisma;
+		m_Athletics.Parent      = m_Strength;
+		m_Acrobatics.Parent     = m_SleightOfHand.Parent = m_Stealth.Parent       = m_Dexterity;
+		m_Arcana.Parent         = m_History.Parent       = m_Investigation.Parent = m_Nature.Parent     = m_Religion.Parent = m_Intelligence;
+		m_AnimalHandling.Parent = m_Insight.Parent       = m_Medicine.Parent      = m_Perception.Parent = m_Survival.Parent = m_Wisdom;
+		m_Deception.Parent      = m_Intimidation.Parent  = m_Performance.Parent   = m_Persuasion.Parent = m_Charisma;
 
 		std::vector<std::reference_wrapper<Skill> > skills = { m_Acrobatics, m_AnimalHandling, m_Arcana, m_Athletics, m_Deception, m_History, m_Insight,
 			m_Intimidation, m_Investigation, m_Medicine, m_Nature, m_Perception, m_Performance, m_Persuasion, m_Religion,
@@ -399,9 +339,7 @@ namespace dnd {
 
 			// If the character is proficient in a skill, add proficiency bonus to the modifier
 			if (skills[i].get().Proficient)
-			{
 				skills[i].get().Modifier += m_ProficiencyBonus;
-			}
 
 			// Add other proficiencies
 		}
@@ -473,29 +411,25 @@ namespace dnd {
 
 	void Character::GenerateSavingThrowModifiers()
 	{
-		m_StrengthSave.Parent = m_Strength;
-		m_DexteritySave.Parent = m_Dexterity;
+		m_StrengthSave.Parent     = m_Strength;
+		m_DexteritySave.Parent    = m_Dexterity;
 		m_ConstitutionSave.Parent = m_Constitution;
 		m_IntelligenceSave.Parent = m_Intelligence;
-		m_WisdomSave.Parent = m_Wisdom;
-		m_CharismaSave.Parent = m_Charisma;
+		m_WisdomSave.Parent       = m_Wisdom;
+		m_CharismaSave.Parent     = m_Charisma;
 
 		std::vector<std::reference_wrapper<Skill> > saves = { m_StrengthSave, m_DexteritySave, m_ConstitutionSave, m_IntelligenceSave,
 			m_WisdomSave, m_CharismaSave };
 
 		// Start at parent ability's modifier
 		for (size_t i = 0; i < saves.size(); ++i)
-		{
 			saves[i].get().Modifier = saves[i].get().Parent.Modifier;
-		}
 
 		// Add profiency bonus if proficient in saving throw
 		for (size_t i = 0; i < saves.size(); ++i)
 		{
 			if (saves[i].get().Proficient)
-			{
 				saves[i].get().Modifier += m_ProficiencyBonus;
-			}
 		}
 	}
 
@@ -505,9 +439,7 @@ namespace dnd {
 		m_PassiveWisdom = m_Wisdom.Modifier + 10;
 
 		if (m_Perception.Proficient)
-		{
 			m_PassiveWisdom += m_ProficiencyBonus;
-		}
 	}
 
 
@@ -534,29 +466,23 @@ namespace dnd {
 		m_Languages.push_back("Common"); // All characters can speak, read, and write in the Common tongue
 
 		if (m_Race == "Dwarf" || m_Race == "Hill Dwarf" || m_Race == "Mountain Dwarf")
-		{
 			m_Languages.push_back("Dwarvish");
-		}
+
 		else if (m_Race == "Elf" || m_Race == "High Elf" || m_Race == "Wood Elf" || m_Race == "Dark Elf (Drow)")
 		{
 			m_Languages.push_back("Elvish");
 
 			// High Elves have the Extra Language Trait
 			if (m_Race == "High Elf")
-			{
-				// 1 is the minimum number to avoid picking Elvish twice
-				m_Languages.push_back(Languages[Random::Int(1, NumLanguages - 1)]);
-			}
+				m_Languages.push_back(Languages[Random::Int(1, NumLanguages - 1)]); // 1 is the minimum number to avoid picking Elvish twice
 		}
+
 		else if (m_Race == "Halfling" || m_Race == "Lightfoot Halfling" || m_Race == "Stout Halfling")
-		{
 			m_Languages.push_back("Halfling");
-		}
+
+		// Humans can pick any language
 		else if (m_Race == "Human")
-		{
-			// Humans can pick any language
 			m_Languages.push_back(Languages[Random::Int(0, NumLanguages - 1)]);
-		}
 	}
 
 
@@ -567,22 +493,13 @@ namespace dnd {
 	void Character::GenerateHitDice()
 	{
 		if (m_Class == "Barbarian")
-		{
 			m_HitDice.Type = "d12";
-		}
 		else if (m_Class == "Fighter" || m_Class == "Paladin" || m_Class == "Ranger")
-		{
 			m_HitDice.Type = "d10";
-		}
-		else if (m_Class == "Bard" || m_Class == "Cleric" || m_Class == "Druid" || m_Class == "Monk"
-			|| m_Class == "Rogue" || m_Class == "Warlock")
-		{
+		else if (m_Class == "Bard" || m_Class == "Cleric" || m_Class == "Druid" || m_Class == "Monk" || m_Class == "Rogue" || m_Class == "Warlock")
 			m_HitDice.Type = "d8";
-		}
 		else if (m_Class == "Sorceror" || m_Class == "Wizard")
-		{
 			m_HitDice.Type = "d6";
-		}
 
 		m_HitDice.Number = m_Level;
 	}
@@ -591,43 +508,25 @@ namespace dnd {
 	{
 		// At level 1, characters start with the max roll of their hit die plus their constitution modifier
 		if (m_Class == "Barbarian")
-		{
 			m_MaxHitPoints = 12 + m_Constitution.Modifier;
-		}
 		else if (m_Class == "Fighter" || m_Class == "Paladin" || m_Class == "Ranger")
-		{
 			m_MaxHitPoints = 10 + m_Constitution.Modifier;
-		}
-		else if (m_Class == "Bard" || m_Class == "Cleric" || m_Class == "Druid" || m_Class == "Monk"
-			|| m_Class == "Rogue" || m_Class == "Warlock")
-		{
+		else if (m_Class == "Bard" || m_Class == "Cleric" || m_Class == "Druid" || m_Class == "Monk" || m_Class == "Rogue" || m_Class == "Warlock")
 			m_MaxHitPoints = 8 + m_Constitution.Modifier;
-		}
 		else if (m_Class == "Sorcerer" || m_Class == "Wizard")
-		{
 			m_MaxHitPoints = 6 + m_Constitution.Modifier;
-		}
 
 		// Health increases with level based on a hit die roll and constitution modifier
 		if (m_Level > 1)
 		{
 			if (m_Class == "Barbarian")
-			{
 				m_MaxHitPoints += Random::Int(1, 12) + m_Constitution.Modifier;
-			}
 			else if (m_Class == "Fighter" || m_Class == "Paladin" || m_Class == "Ranger")
-			{
 				m_MaxHitPoints += Random::Int(1, 10) + m_Constitution.Modifier;
-			}
-			else if (m_Class == "Bard" || m_Class == "Cleric" || m_Class == "Druid" || m_Class == "Monk"
-				|| m_Class == "Rogue" || m_Class == "Warlock")
-			{
+			else if (m_Class == "Bard" || m_Class == "Cleric" || m_Class == "Druid" || m_Class == "Monk" || m_Class == "Rogue" || m_Class == "Warlock")
 				m_MaxHitPoints += Random::Int(1, 8) + m_Constitution.Modifier;
-			}
 			else if (m_Class == "Sorcerer" || m_Class == "Wizard")
-			{
 				m_MaxHitPoints += Random::Int(1, 6) + m_Constitution.Modifier;
-			}
 		}
 
 		// Add other modifiers
@@ -635,16 +534,10 @@ namespace dnd {
 
 	void Character::GenerateSpeed()
 	{
-		if (m_Race == "Dwarf" || m_Race == "Hill Dwarf" || m_Race == "Mountain Dwarf"
-			|| m_Race == "Halfling" || m_Race == "Lightfoot Halfling" || m_Race == "Stout Halfling")
-		{
+		if (m_Race == "Dwarf" || m_Race == "Hill Dwarf" || m_Race == "Mountain Dwarf" || m_Race == "Halfling" || m_Race == "Lightfoot Halfling" || m_Race == "Stout Halfling")
 			m_Speed = 25;
-		}
-		else if (m_Race == "Elf" || m_Race == "High Elf" || m_Race == "Wood Elf" || m_Race == "Dark Elf (Drow)"
-			|| m_Race == "Human")
-		{
+		else if (m_Race == "Elf" || m_Race == "High Elf" || m_Race == "Wood Elf" || m_Race == "Dark Elf (Drow)" || m_Race == "Human")
 			m_Speed = 30;
-		}
 	}
 
 	void Character::GenerateInitiative()
@@ -671,35 +564,34 @@ namespace dnd {
 	{
 		std::cout << std::boolalpha;
 
-		std::cout << "Name: " << m_FirstName << " " << m_Surname << "\n";
-		std::cout << "Sex: " << m_Gender << "\n";
-		std::cout << "Race: " << m_Race << "\n";
+		std::cout << "Name: "   << m_FirstName << " " << m_Surname << "\n";
+		std::cout << "Gender: " << m_Gender    << "\n";
+		std::cout << "Race: "   << m_Race      << "\n";
+
 		if (m_Race == "Human")
-		{
 			std::cout << "Ethnicity: " << m_Ethnicity << "\n";
-		}
-		std::cout << "Class: " << m_Class << "\n";
-		std::cout << "Level: " << m_Level << "\n";
-		std::cout << "Experience Points: " << m_Experience << "\n";
-		std::cout << "Proficiency Bonus: " << m_ProficiencyBonus << "\n";
-		std::cout << "Hit Points: " << m_MaxHitPoints << "\n";
-		std::cout << "Speed: " << m_Speed << "\n";
-		std::cout << "Initiative: " << m_Initiative << "\n";
-		std::cout << "Background: " << m_Background << "\n";
-		std::cout << "Alignment: " << m_Alignment << "\n\n";
+
+		std::cout << "Class: "              << m_Class << "\n";
+		std::cout << "Level: "              << m_Level << "\n";
+		std::cout << "Experience Points: "  << m_Experience << "\n";
+		std::cout << "Proficiency Bonus: "  << m_ProficiencyBonus << "\n";
+		std::cout << "Hit Points: "         << m_MaxHitPoints << "\n";
+		std::cout << "Speed: "              << m_Speed << "\n";
+		std::cout << "Initiative: "         << m_Initiative << "\n";
+		std::cout << "Background: "         << m_Background << "\n";
+		std::cout << "Alignment: "          << m_Alignment << "\n\n";
 		std::cout << "Personality Traits: " << m_PersonalityTraits << "\n\n";
-		std::cout << "Ideals: " << m_Ideals << "\n\n";
-		std::cout << "Bonds: " << m_Bonds << "\n\n";
-		std::cout << "Flaws: " << m_Flaws << "\n\n";
+		std::cout << "Ideals: "             << m_Ideals << "\n\n";
+		std::cout << "Bonds: "              << m_Bonds << "\n\n";
+		std::cout << "Flaws: "              << m_Flaws << "\n\n";
 
 		std::cout << "Languages: ";
 		for (size_t i = 0; i < m_Languages.size(); ++i)
 		{
 			std::cout << m_Languages[i];
 			if (i != m_Languages.size() - 1)
-			{
 				std::cout << ", ";
-			}
+
 		}
 		std::cout << "\n\n";
 
@@ -707,41 +599,41 @@ namespace dnd {
 		std::cout << "Abilites\n";
 		std::cout << "========\n";
 		std::cout << "Strength\n";
-		std::cout << "Score: " << m_Strength.Score << "\n";
+		std::cout << "Score:    " << m_Strength.Score << "\n";
 		std::cout << "Modifier: " << m_Strength.Modifier << "\n\n";
 		std::cout << "Dexterity\n";
-		std::cout << "Score: " << m_Dexterity.Score << "\n";
+		std::cout << "Score:    " << m_Dexterity.Score << "\n";
 		std::cout << "Modifier: " << m_Dexterity.Modifier << "\n\n";
 		std::cout << "Constitution\n";
-		std::cout << "Score: " << m_Constitution.Score << "\n";
+		std::cout << "Score:    " << m_Constitution.Score << "\n";
 		std::cout << "Modifier: " << m_Constitution.Modifier << "\n\n";
 		std::cout << "Intelligence\n";
-		std::cout << "Score: " << m_Intelligence.Score << "\n";
+		std::cout << "Score:    " << m_Intelligence.Score << "\n";
 		std::cout << "Modifier: " << m_Intelligence.Modifier << "\n\n";
 		std::cout << "Wisdom\n";
-		std::cout << "Score: " << m_Wisdom.Score << "\n";
+		std::cout << "Score:    " << m_Wisdom.Score << "\n";
 		std::cout << "Modifier: " << m_Wisdom.Modifier << "\n\n";
 		std::cout << "Charisma\n";
-		std::cout << "Score: " << m_Charisma.Score << "\n";
+		std::cout << "Score:    " << m_Charisma.Score << "\n";
 		std::cout << "Modifier: " << m_Charisma.Modifier << "\n\n";
 
 		std::cout << "=============\n";
 		std::cout << "Saving Throws\n";
 		std::cout << "=============\n";
 		std::cout << "Proficiencies\n";
-		std::cout << "Strength: " << m_StrengthSave.Proficient << "\n";
-		std::cout << "Dexterity: " << m_DexteritySave.Proficient << "\n";
+		std::cout << "Strength:     " << m_StrengthSave.Proficient << "\n";
+		std::cout << "Dexterity:    " << m_DexteritySave.Proficient << "\n";
 		std::cout << "Constitution: " << m_ConstitutionSave.Proficient << "\n";
 		std::cout << "Intelligence: " << m_IntelligenceSave.Proficient << "\n";
-		std::cout << "Wisdom: " << m_WisdomSave.Proficient << "\n";
-		std::cout << "Charisma: " << m_CharismaSave.Proficient << "\n\n";
+		std::cout << "Wisdom:       " << m_WisdomSave.Proficient << "\n";
+		std::cout << "Charisma:     " << m_CharismaSave.Proficient << "\n\n";
 		std::cout << "Modifiers\n";
-		std::cout << "Strength: " << m_StrengthSave.Modifier << "\n";
-		std::cout << "Dexterity: " << m_DexteritySave.Modifier << "\n";
+		std::cout << "Strength:     " << m_StrengthSave.Modifier << "\n";
+		std::cout << "Dexterity:    " << m_DexteritySave.Modifier << "\n";
 		std::cout << "Constitution: " << m_Constitution.Modifier << "\n";
 		std::cout << "Intelligence: " << m_IntelligenceSave.Modifier << "\n";
-		std::cout << "Wisdom: " << m_WisdomSave.Modifier << "\n";
-		std::cout << "Charisma: " << m_CharismaSave.Modifier << "\n\n";
+		std::cout << "Wisdom:       " << m_WisdomSave.Modifier << "\n";
+		std::cout << "Charisma:     " << m_CharismaSave.Modifier << "\n\n";
 
 		std::cout << "======\n";
 		std::cout << "Skills\n";
@@ -765,6 +657,7 @@ namespace dnd {
 		std::cout << "Sleight of Hand: " << m_SleightOfHand.Proficient << "\n";
 		std::cout << "Stealth:         " << m_Stealth.Proficient << "\n";
 		std::cout << "Survival:        " << m_Survival.Proficient << "\n\n";
+
 		std::cout << "Modifiers\n";
 		std::cout << "Acrobatics:      " << m_Acrobatics.Modifier << "\n";
 		std::cout << "Animal Handling: " << m_AnimalHandling.Modifier << "\n";
